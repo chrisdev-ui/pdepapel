e1a1798174ac3a1aea3ce952a3a6881b6fd93ac0
chrisdev-ui <web.christian.dev@gmail.com>
2023-10-15 03:31:52 -0500

    feat: setup project and start to build admin dashboard

---
+++ pdepapel-admin/app/layout.tsx
@@ -0,0 +1,32 @@
+import { ModalProvider } from '@/providers/modal-provider'
+import { Toaster } from '@/providers/toaster'
+import { esES } from '@clerk/localizations'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'PdePapel Admin Dashboard',
+  description: 'Admin Dashboard for PdePapel'
+}
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider localization={esES}>
+      <html lang="es">
+        <body className={inter.className}>
+          <Toaster />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
---
+++ pdepapel-admin/components/modals/store-modal.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import { useStoreModal } from '@/hooks/use-store-modal'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Modal } from '@/components/ui/modal'
+import { useToast } from '@/hooks/use-toast'
+import axios from 'axios'
+
+const formSchema = z.object({
+  name: z.string().min(1, 'El nombre debe tener al menos un caracter')
+})
+
+export function StoreModal() {
+  const storeModal = useStoreModal()
+  const { toast } = useToast()
+
+  const [loading, setLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: ''
+    }
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true)
+      const response = await axios.post('/api/stores', values)
+      window.location.assign(`/${response.data.id}`)
+    } catch (error) {
+      toast({
+        description:
+          '¡Ups! Algo salió mal. Por favor, verifica tu conexión e inténtalo nuevamente más tarde.',
+        variant: 'destructive'
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+  return (
+    <Modal
+      title="Crea una tienda"
+      description="Agrega una nueva tienda para manejar productos y categorias"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Nombre de la tienda"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  )
+}
