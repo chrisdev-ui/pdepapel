aa06dea1f9b26f466de73d60e00af0fa72517883
chrisdev-ui <web.christian.dev@gmail.com>
2023-10-18 21:10:20 -0500

    feat: improve code in navigation link

--- pdepapel-store/components/navigation-link.tsx
+++ pdepapel-store/components/navigation-link.tsx
@@ -19,12 +19,14 @@ export const NavigationLink: React.FC<NavigationLinkProps> = ({
   return (
     <Link
       href={href}
-      className={cn("relative font-serif font-semibold no-underline", {
-        "after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-blue-yankees after:content-['']":
-          active,
-        "after:w-0 after:transition-all after:duration-300 after:ease-in-out lg:hover:after:absolute lg:hover:after:bottom-0 lg:hover:after:left-0 lg:hover:after:h-0.5 lg:hover:after:w-full lg:hover:after:bg-blue-yankees lg:hover:after:content-['']":
-          !active,
-      })}
+      className={cn(
+        "relative font-serif font-semibold no-underline after:absolute after:bottom-0 after:left-0 after:h-0.5 after:bg-blue-yankees after:content-['']",
+        {
+          "after:w-full": active,
+          "after:w-0 after:transition-all after:duration-300 after:ease-in-out lg:hover:after:w-full":
+            !active,
+        },
+      )}
     >
       {children}
     </Link>
