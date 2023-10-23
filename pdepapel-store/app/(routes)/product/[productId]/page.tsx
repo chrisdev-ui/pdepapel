eb18169d6cd4d181c8469190014d1bf2e2c85f1d
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2023-10-23 01:45:41 -0500

    feat: add newsletter to individual product page

--- pdepapel-store/app/(routes)/product/[productId]/page.tsx
+++ pdepapel-store/app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { getProduct } from "@/actions/get-product";
 import { getProducts } from "@/actions/get-products";
 import { Gallery } from "@/components/gallery";
 import { Info } from "@/components/info";
+import { Newsletter } from "@/components/newsletter";
 import { ProductList } from "@/components/product-list";
 import { Container } from "@/components/ui/container";
 import { Separator } from "@/components/ui/separator";
@@ -19,18 +20,21 @@ export default async function ProductPage({ params }: ProductPageProps) {
     limit: 4,
   });
   return (
-    <Container className="px-4 py-10 sm:px-6 lg:px-8">
-      <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-        <Gallery images={product.images} />
-        <div className="mt-10 px-4 sm:mt-6 sm:px-0 lg:mt-0">
-          <Info data={product} />
+    <>
+      <Container className="px-4 py-10 sm:px-6 lg:px-8">
+        <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+          <Gallery images={product.images} />
+          <div className="mt-10 px-4 sm:mt-6 sm:px-0 lg:mt-0">
+            <Info data={product} />
+          </div>
         </div>
-      </div>
-      <Separator className="my-10" />
-      <ProductList
-        title="Productos relacionados"
-        products={suggestedProducts}
-      />
-    </Container>
+        <Separator className="my-10" />
+        <ProductList
+          title="Productos relacionados"
+          products={suggestedProducts}
+        />
+      </Container>
+      <Newsletter />
+    </>
   );
 }
