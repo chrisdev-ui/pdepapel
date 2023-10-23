bffe2663fd0a25f2568925e8c8d1604e2641fb6b
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2023-10-23 09:16:29 -0500

    feat: fix max width

--- pdepapel-store/app/(routes)/product/[productId]/page.tsx
+++ pdepapel-store/app/(routes)/product/[productId]/page.tsx
@@ -21,7 +21,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
   });
   return (
     <>
-      <Container className="px-4 py-10 sm:px-6 lg:px-8">
+      <Container className="max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
         <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
           <Gallery images={product.images} />
           <div className="mt-10 px-4 sm:mt-6 sm:px-0 lg:mt-0">
