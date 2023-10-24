61c5c68060033326d7a2aba7a9b08803587b7864
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2023-10-24 03:00:58 -0500

    feat: improve stack async calls with promise all

--- pdepapel-store/app/(routes)/page.tsx
+++ pdepapel-store/app/(routes)/page.tsx
@@ -13,11 +13,14 @@ import { Newsletter } from "@/components/newsletter";
 export const revalidate = 0;
 
 export default async function HomePage() {
-  const billboards = await getBillboards();
-  const featureProducts = await getProducts({ isFeatured: true, limit: 8 });
-  const mainBanner = await getMainBanner();
-  const newProducts = await getProducts({ onlyNew: true, limit: 8 });
-  const banners = await getBanners();
+  const [billboards, featureProducts, mainBanner, newProducts, banners] =
+    await Promise.all([
+      getBillboards(),
+      getProducts({ isFeatured: true, limit: 8 }),
+      getMainBanner(),
+      getProducts({ onlyNew: true, limit: 8 }),
+      getBanners(),
+    ]);
   return (
     <>
       <HeroSlider data={billboards} />
