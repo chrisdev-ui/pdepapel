32f7440d2ae1c2601b1accb30d35a7bb8b3ac8fe
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2023-10-25 22:10:44 -0500

    fix: summary button css issues

--- pdepapel-store/app/(routes)/cart/components/summary.tsx
+++ pdepapel-store/app/(routes)/cart/components/summary.tsx
@@ -52,7 +52,9 @@ export const Summary: React.FC<{}> = () => {
         className="group relative mt-6 w-full overflow-hidden rounded-full bg-blue-yankees font-serif text-base font-bold uppercase text-white hover:bg-blue-yankees"
       >
         <CreditCard className="absolute left-0 h-5 w-5 -translate-x-full transform transition-transform duration-500 ease-out group-hover:translate-x-64" />
-        <span className="group-hover:hidden">Finalizar compra</span>
+        <span className="transition-opacity duration-150 group-hover:opacity-0">
+          Finalizar compra
+        </span>
       </Button>
     </div>
   );
