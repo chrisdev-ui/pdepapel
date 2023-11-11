127604dff77c3e9a99a835afcd7509071ee925a4
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2023-11-11 12:26:42 -0500

    fix: modify get total revenue calculation

--- pdepapel-admin/actions/get-total-revenue.ts
+++ pdepapel-admin/actions/get-total-revenue.ts
@@ -18,7 +18,7 @@ export const getTotalRevenue = async (storeId: string) => {
 
   const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + Number(item.product.price)
+      return orderSum + Number(item.product.price) * item.quantity
     }, 0)
     return total + orderTotal
   }, 0)
