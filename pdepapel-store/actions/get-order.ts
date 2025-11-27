986db41d9d7af2569d66aaf6871df32cbea37cdb
christorres-dev <christian.gabriel.torres@depalmastudios.com>
2025-11-27 00:17:04 -0500

    feat: add `no-store` cache option to order fetching.

--- pdepapel-store/actions/get-order.ts
+++ pdepapel-store/actions/get-order.ts
@@ -4,6 +4,8 @@ import { Order } from "@/types";
 const API_URL = `${env.NEXT_PUBLIC_API_URL}/orders`;
 
 export const getOrder = async (id: string): Promise<Order> => {
-  const response = await fetch(`${API_URL}/${id}`);
+  const response = await fetch(`${API_URL}/${id}`, {
+    cache: "no-store",
+  });
   return response.json();
 };
